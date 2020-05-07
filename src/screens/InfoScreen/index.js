8ae8778e033b3650b260c1872c0b818ad3b1eb5c
José Wilson <wilsoncx@gmail.com>
2020-05-07 02:06:03 -0300

    Atualização de informações
    Atualização de informações

--- src/screens/InfoScreen/index.js
+++ src/screens/InfoScreen/index.js
@@ -20,11 +20,10 @@ const InfoScreen = () => (
           pandemia do COVID-19. Dados obtidos pelo projeto covid19-brazil-api
           (https://github.com/devarthurribeiro/covid19-brazil-api) o mesmo usa
           como base os dados do CSSEGIS, Ministério da Saúde Coronavírus Brasil.
-          utilizarmos também o projeto API Dataset covid19 no
+          Utilizarmos também o projeto API Dataset covid19 no
           Brasil.IO(https://github.com/turicas/covid19-br) que utiliza como
           Fonte: Secretarias de Saúde das Unidades Federativas, dados tratados
-          por Álvaro Justen e colaboradores/Brasil.IO. Desenvolvido por José
-          Wilson (@eusouwilson)
+          por Álvaro Justen e colaboradores/Brasil.IO.
         </Content>
         <Content>Desenvolvido por José Wilson (@eusouwilson)</Content>
       </>
--- src/screens/InfoScreen/styles.info.js
+++ src/screens/InfoScreen/styles.info.js
@@ -1,9 +1,15 @@
 import styled from 'styled-components/native';
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from 'react-native-responsive-screen';
 
 export const Container = styled.View`
   flex: 1;
 `;
 export const Header = styled.View`
+  border-bottom-right-radius: 30px;
+  border-bottom-left-radius: 30px;
   padding-left: 18px;
   padding-right: 18px;
   padding-top: 100px;
@@ -12,21 +18,27 @@ export const Header = styled.View`
 
 export const ContainerTitle = styled.View`
   border-bottom-width: 0.3px;
+  border-bottom-color: #fff;
   margin-bottom: 20px;
 `;
 
 export const Title = styled.Text`
-  font-size: 16px;
-  font-weight: 700;
+  font-size: ${hp('2.5%')}px;
+  font-weight: 600;
+  font-style: normal;
   text-align: left;
   color: #fff;
   margin-bottom: 20px;
+  line-height: 20px;
+  letter-spacing: -0.36px;
 `;
 
 export const Content = styled.Text`
-  font-size: 14px;
-  font-weight: 600;
+  font-size: ${hp('1.5%')}px;
+  font-weight: 300;
   text-align: justify;
   color: #fff;
   margin-bottom: 20px;
+  line-height: 16px;
+  letter-spacing: -0.28px;
 `;
